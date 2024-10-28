9247620808e428b29c6ca566a36f4ec63bf1acfe
WolfTech Innovations <SpoinkOSGithub@gmail.com>
2024-10-28 13:35:03 -0500

    Update MLMLS.hpp

--- src/MLMLS.hpp
+++ src/MLMLS.hpp
@@ -2,11 +2,21 @@
 #define MLMLS_HPP
 
 #include <string>
+#include <iostream>
 
 class MLMLS {
 public:
-    std::string requestData(const std::string &resource);
-    std::string receiveData(const std::string &data);
+    std::string requestData(const std::string &resource) {
+        return "MLMLS_REQUEST:" + resource;
+    }
+
+    void receiveData(const std::string &data) {
+        if (data.rfind("MLMLS_REQUEST:", 0) == 0) {
+            std::cout << "Received MLMLS data for resource: " << data.substr(14) << std::endl;
+        } else {
+            std::cout << "Invalid data format received on MLMLS." << std::endl;
+        }
+    }
 };
 
 #endif
